2e66acf5e8c2bfbb2f48edd464e1910473ea0812
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2025-02-20 11:35:12 -0800

    finsihed #2 and #3

--- assignment.py
+++ assignment.py
@@ -1,29 +1,35 @@
 import sqlite3
-
+import re
 ids = []
 connection = sqlite3.connect('dbase.db')
 cursor = connection.cursor()
 
-q = 'pragma table_info(npc);'
-cursor.execute(q)
-result = cursor.fetchall()
-#print(result)
-
-#q = 'select * from npc where level<3 order by level desc;'
-#q = 'select * from npc where level>3 and level < 6 order by level asc limit 20;'
-#q = "select * from npc where class='Samurai' order by strength asc"
-#q = "select * from npc where class like '%an%'"
-#q = "select * from npc where hp>20 and class='Ranger'"
-#q = "select * from npc where gold>30 and (class = 'Ranger' or class='Samurai')"
-#q = "select id,class,level from npc where gold>50"
-#q = "select id from npc where gold>50"
-
-#1
-cursor.execute(q)
+#1 table structure
+print("1.")
+cursor.execute('pragma table_info(npc);')
 result = cursor.fetchall()
 for i in result:
     print(i)
 
-print("1. " + 'pragma table_info(npc);')
-
 #2
+print("\n2.")
+cursor.execute('select id from npc;')
+a = str(cursor.fetchall()[-1])
+a = re.sub('[(),]', '', a)
+print(a)
+
+#3
+print("\n3.")
+cursor.execute("select class from npc where class='Knight'")
+print(len(cursor.fetchall()))
+
+#4
+print("\n4.")
+cursor.execute("select class from npc")
+b = cursor.fetchall()
+b.sort()
+for i in b:
+    i = str(i)
+    
+Class = ["('Warrior',)","('Ranger',)","('Knight',)","('Sorcerer',)","('Sage',)","('Priest',)","('Thief',)","('Bard',)","('Barbarian',)","('Monk',)","('Assassin',)","('Jester',)","('Samurai',)"]
+
